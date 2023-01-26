b30bdab7335f2e9667240ccf4cd7d9f04038d307
Travless <travis.white0101@gmail.com>
2023-01-26 14:32:32 -0500

    Add additional functions to Tree class, then provide testing for functions to confirm their functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Class Creation
+
 class Node {
     constructor(data, left = null, right = null){
         this.data = data;
@@ -7,12 +9,31 @@ class Node {
 }
 
 class Tree {
-    constructor() {
-        this.root = null;
+    constructor(array) {
+        const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+        this.root = this.buildTree(sortedArray);
     }
 
-    add(data) {
+    // function that builds binary tree off of a provided array of data
+    buildTree(sortedArray){
+        // loop through each array element, taking it's data and using the insert function to indert it into binary tree nodes and place it appropriately within the tree
+        if(sortedArray.length === 0){
+            return null;
+        }
+        let mid = Math.floor(sortedArray.length / 2);
+        let root = new Node(sortedArray[mid]);
+
+        root.left = this.buildTree(sortedArray.slice(0, mid));
+        root.right = this.buildTree(sortedArray.slice(mid+1));
+
+        return root;
+    }
+
+    // add node to BST
+    insert(data) {
+        // assign the root node to the node variable
         const node = this.root;
+        // if no root node exisits (meaning no tree exisits), assign a new node created with the node class to the root node;
         if (node === null){
             this.root = new Node(data);
             return;
@@ -110,7 +131,7 @@ class Tree {
     }
 
     // will remove the equivalent data passed into the function from the BST
-    remove(data){
+    delete(data){
         // create function that will be used recursively to locate node based on data passed in
         const removeNode = function(node, data) {
             // if the node is null (i.e. empty tree), return null
@@ -160,40 +181,226 @@ class Tree {
         this.root = removeNode(this.root, data)
     }
 
+    // will return the minimum level height of the binary tree
+    // pass in node, or if a node isn't passed in, the functions assigns the root node to the node
+    minHeight(node = this.root){
+        // check if node is null. if it is, return -1
+        if(node == null) {
+            return -1;
+        }
+        // assign the function recursively to two different variables, left and right, while passing in the left and right data of the node
+        let left = this.minHeight(node.left);
+        let right = this.minHeight(node.right);
+        // compare both nodes to see if left or right is less than its counterpart, and if it is, return the node plus 1
+        if(left < right){
+            return left + 1;
+        } else {
+            return right + 1;
+        };
+    }
+
+    // will return the maximum level height of the binary search tree
+    // pass in node, or if a node isn't passed in, the function assigns the root node to the node
+    maxHeight(node = this.root){
+        // check if node is null. if it is, return -1
+        if (node == null){
+            return -1;
+        }
+        // assign the function recursively to two different variables, left and right, while passing in the left and right data of the node
+        let left = this.maxHeight(node.left);
+        let right = this.maxHeight(node.right);
+        // compare both nodes to see if left or right is greater than its counterpart, and if it is, return the node plus 1
+        if(left > right){
+            return left + 1;
+        } else {
+            return right + 1;
+        };
+    }
+
     isBalanced(){
-        // 
+        // will return true or false if the minHeight of the binary tree is greater than or equal to the maxHeight of the tree minus 1
         return (this.minHeight() >= this.maxHeight() - 1);
+    }
+
+    rebalance(){
+        let results = this.inOrder()
+        this.root = this.buildTree(results);
+    }
+
+
+    // Depth-First Order
+    inOrder(){
+        // check if BST exisits
+        if(this.root == null){
+            return null;
+        } else {
+            // if there is a BST, create a new array and add each value found via inOrder to the array
+            let result = new Array();
+            // create function traversalInOrder that takes in a node and that will be recursive in checking and adding data to the result array
+            // inorder = Left -> Data -> Right (LDR)
+            function traversalInOrder(node){
+                // use short circut evaluation to check if node.left exisits and if it does exisit, run the traversalInOrder function on node.left
+                node.left && traversalInOrder(node.left);
+                // push data from node to result array
+                result.push(node.data);
+                 // use short circut evaluation to check if node.right exisits and if it does exisit, run the traversalInOrder function on node.right
+                node.right && traversalInOrder(node.right);
+            }
+            // run traversalInOrder function on the root node and resturn the result
+            traversalInOrder(this.root);
+            return result;
+        }
+    }
+
+    preOrder(){
+        // check if BST exisits
+        if(this.root == null){
+            return null;
+        } else {
+            // if there is a BST, create a new array and add each value found via inOrder to the array
+            let result = new Array();
+            // create function traversalInOrder that takes in a node and that will be recursive in checking and adding data to the result array
+            // preorder = Data -> Left -> Right (DLR)
+            function traversalInOrder(node){
+                // push data from node to result array
+                result.push(node.data);
+                // use short circut evaluation to check if node.left exisits and if it does exisit, run the traversalInOrder function on node.left
+                node.left && traversalInOrder(node.left);
+                 // use short circut evaluation to check if node.right exisits and if it does exisit, run the traversalInOrder function on node.right
+                node.right && traversalInOrder(node.right);
+            }
+            // run traversalInOrder function on the root node and resturn the result
+            traversalInOrder(this.root);
+            return result;
+        }
+    }
+
+    postOrder(){
+        if(this.root == null){
+            return null;
+        } else {
+            // if there is a BST, create a new array and add each value found via inOrder to the array
+            let result = new Array();
+            // create function traversalInOrder that takes in a node and that will be recursive in checking and adding data to the result array
+            // postorder = Left - Right - Data (LRD)
+            function traversalInOrder(node){
+                // use short circut evaluation to check if node.left exisits and if it does exisit, run the traversalInOrder function on node.left
+                node.left && traversalInOrder(node.left);
+                 // use short circut evaluation to check if node.right exisits and if it does exisit, run the traversalInOrder function on node.right
+                node.right && traversalInOrder(node.right);
+                // push data from node to result array
+                result.push(node.data);
+            }
+            // run traversalInOrder function on the root node and resturn the result
+            traversalInOrder(this.root);
+            return result;
+        }
+    }
+
+    // records the count of edges within the BST
+    // set the node variable to the root node and the edge count to 0 when initialized
+    depth(nodeValue, node = this.root, edge = 0){
+        // if no BST exisits, return null
+        if (node === null){
+            return;
+        }
+        // if the value of the node's data equals the nodeValue variable, return the edge count
+        if (node.value === nodeValue){
+            return edge;
+        }
+        // if the value of the node is less than the nodeValue variable, recursively execute the depth functions, with nodeValue, node.right, and the edge count increased by 1 passed into it
+        if (node.value < nodeValue){
+            return this.depth(nodeValue, node.right, edge++);
+        // if the value of the node is greater than the nodeValue variable, recursively execute the depth functions, with nodeValue, node.left, and the edge count increased by 1 passed into it
+        } else {
+            return this.depth(nodeValue, node.left, edge++);
+        }
+    }
+
+
+    // Breadth-First Order
+
+    // use queue to help while looping through nodes, dequeueing as the nodes are pushed to a results array
+    levelOrder(){
+        // create results array to put sorted nodes based on level order, and create a queue array to house nodes during sorting process
+        let results = [];
+        let queue = [];
+        // check if there is a BST by checking if the root node isn't null
+        // if it isn't null, push the root node into the queue
+        if(this.root != null){
+            queue.push(this.root);
+            // begin looping through elements adding them to the tree based on their location on the tree at that level
+            while(queue.length > 0){
+                // use shift function to return the first element of the queue and assign it to the node variable
+                let node = queue.shift();
+                // push the data of that returned node from the queue into the results array
+                results.push(node.data);
+                // if the node has a left child, push that node data into the queue
+                if (node.left != null){
+                    queue.push(node.left);
+                };
+                // if the node has a right child, push that node data into the queue
+                if (node.right != null){
+                    queue.push(node.right);
+                };
+            };
+            // when the loop finishes, meaning the queue has no remaining nodes, return the results array
+            return results;
+        } else {
+            return null;
+        };
+    };
+}
+
+
+// Ancillary functions
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
 }
 
-// const prettyPrint = (node, prefix = '', isLeft = true) => {
-//     if (node.right !== null) {
-//       prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-//     }
-//     console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-//     if (node.left !== null) {
-//       prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-//     }
-// }
+const arrayRandomizer = (length) => {
+    return Array.from({ length: length }, () => Math.floor(Math.random() * 100))
+}
 
-// const arrayTest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// const newTree = new Tree(arrayTest);
 
-// newTree.buildTree(arrayTest, 0, 9);
+// TESTING
 
-const bst = new Tree();
+// Creaate BST from random array
+const randomArray = arrayRandomizer(10);
+const bst = new Tree(randomArray);
 
-bst.add(4);
-bst.add(2);
-bst.add(6);
-bst.add(1);
-bst.add(3);
-bst.add(5);
-bst.add(7);
-bst.remove(4);
-console.log(bst.findMin());
-console.log(bst.findMax());
-bst.remove(7);
-console.log(bst.findMax());
-console.log(bst.isPresent(4));
+// Confirm that tree is balanced
+console.log(bst.isBalanced());
+
+// Print our all elements in level, pre, post, and in order
+console.log(bst.levelOrder());
+console.log(bst.preOrder());
+console.log(bst.postOrder());
+console.log(bst.inOrder());
+
+
+// Unbalance tree by additing numbers
+bst.insert(300);
+bst.insert(600);
+
+// Confirm that tree is unbalanced
+console.log(bst.isBalanced());
+
+// Rebalance tree
+bst.rebalance();
+
+// Confirm that tree is balanced
+console.log(bst.isBalanced())
+
+// Print out elements in level, pre, post, and in order again
+console.log(bst.levelOrder());
+console.log(bst.preOrder());
+console.log(bst.postOrder());
+console.log(bst.inOrder());
